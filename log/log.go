5a51e04751a6d353d117114c2e67c902fcfb1dc9
Tom Deckers <tom@ducbase.com>
2014-11-01 18:16:14 +0100

    complete logging

--- log/log.go
+++ log/log.go
@@ -1,17 +1,75 @@
 package log
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"time"
 )
 
 var (
-	tag = string
+	tag     string
+	quiet   = false
+	verbose = false
+	debug   = false
 )
 
 func init() {
 	tag = os.Args[0]
+}
+
+// SetTag sets the tag used for logging.
+func SetTag(t string) {
+	tag = t
+}
+
+// SetQuiet sets quiet mode.
+func SetQuiet(enable bool) {
+	quiet = enable
+}
+
+// SetDebug sets debug mode
+func SetDebug(enable bool) {
+	debug = enable
+}
+
+// SetVerbose sets verbose mode.
+func SetVerbose(enable bool) {
+	verbose = enable
+}
+
+// Debug logs a message with severity DEBUG.
+func Debug(msg string) {
+	if debug {
+		log("DEBUG", msg)
+	}
+}
+
+// Error logs a message with severity ERROR.
+func Error(msg string) {
+	log("ERROR", msg)
+}
+
+// Fatal logs a message with severity ERROR and exists by a call to os.Exit().
+func Fatal(msg string) {
+	Error(msg)
+	os.Exit(1)
+}
+
+// Info logs a message with severity INFO.
+func Info(msg string) {
+	log("INFO", msg)
+}
+
+// Notice logs a message with severity NOTICE.
+func Notice(msg string) {
+	if verbose || debug {
+		log("NOTICE", msg)
+	}
+}
+
+// Warning logs a message with severity WARNING.
+func Warning(msg string) {
+	log("WARNING", msg)
 }
 
 func log(level, msg string) {
@@ -23,5 +81,10 @@ func log(level, msg string) {
 		if quiet {
 			return
 		}
+		w = os.Stdout
+	case "ERROR":
+		w = os.Stderr
 	}
+	fmt.Fprintf(w, "%s %s %s[%d]: %s %s\n",
+		timestamp, hostname, tag, os.Getpid(), level, msg)
 }
